d464f990b2de214b726ef149b287d0a34042611f
endeavormac <endeavormac@6d32eb27-d60d-8894-b0e1-8c47104f2c3a>
2010-05-01 23:48:13 +0000

    removing more options that are no longer used with new fuzzer engine

--- cold_fuzz/src/options.c
+++ cold_fuzz/src/options.c
@@ -38,16 +38,7 @@ void button_save_options_click ()
 
 	GObject * object;
 	
-	object = get_ui_object("checkbutton_newline");
-	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(object)) == TRUE)
-		OPTIONS |= OPTIONS_NEWLINE;
-	else
-		OPTIONS &= ~OPTIONS_NEWLINE;
-	
 	set_threadsafe_int(&OPTIONS_THREADS_N, options_get_int_from_entry("box_number_of_threads"));
-	set_threadsafe_int(&OPTIONS_RANDOM_BYTES, options_get_int_from_entry("box_random_bytes"));
-	set_threadsafe_int(&OPTIONS_MIN_LENGTH_RANDOM_BYTES, options_get_int_from_entry("box_min_length_random_bytes"));
-	set_threadsafe_int(&OPTIONS_MAX_LENGTH_RANDOM_BYTES, options_get_int_from_entry("box_max_length_random_bytes"));
 	set_threadsafe_int(&OPTIONS_RESULTS_MAX, options_get_int_from_entry("box_results_max"));
 	
 	object = get_ui_object("options_window");
@@ -62,11 +53,7 @@ void options_init ()
 
 	GObject * object; 
 	
-	OPTIONS = 0;
 	set_threadsafe_int(&OPTIONS_THREADS_N, 5);
-	set_threadsafe_int(&OPTIONS_RANDOM_BYTES, 0);
-	set_threadsafe_int(&OPTIONS_MIN_LENGTH_RANDOM_BYTES, 0);
-	set_threadsafe_int(&OPTIONS_MAX_LENGTH_RANDOM_BYTES, 0);
 	set_threadsafe_int(&OPTIONS_RESULTS_MAX, 100);
 	
 	object = get_ui_object("button_save_options");
@@ -85,9 +72,6 @@ int options_window ()
 	GObject * object;
 	
 	options_set_entry_to_int("box_number_of_threads", get_threadsafe_int(&OPTIONS_THREADS_N));
-	options_set_entry_to_int("box_random_bytes", get_threadsafe_int(&OPTIONS_RANDOM_BYTES));
-	options_set_entry_to_int("box_min_length_random_bytes", get_threadsafe_int(&OPTIONS_MIN_LENGTH_RANDOM_BYTES));
-	options_set_entry_to_int("box_max_length_random_bytes", get_threadsafe_int(&OPTIONS_MAX_LENGTH_RANDOM_BYTES));
 	options_set_entry_to_int("box_results_max", get_threadsafe_int(&OPTIONS_RESULTS_MAX));
 	
 	object = get_ui_object("options_window");
--- cold_fuzz/src/options.h
+++ cold_fuzz/src/options.h
@@ -6,13 +6,7 @@
 #include "interface.h"
 #include "thread.h"
 
-#define OPTIONS_NEWLINE (1 << 0)
-
-unsigned int OPTIONS;
 threadsafe_int_t OPTIONS_THREADS_N;
-threadsafe_int_t OPTIONS_RANDOM_BYTES;
-threadsafe_int_t OPTIONS_MIN_LENGTH_RANDOM_BYTES;
-threadsafe_int_t OPTIONS_MAX_LENGTH_RANDOM_BYTES;
 threadsafe_int_t OPTIONS_RESULTS_MAX;
 
 
